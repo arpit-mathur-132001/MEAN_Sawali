14d79e5c2593540144d4db1399887a13f77e255d
SixFingure <64139109+SixFingure@users.noreply.github.com>
2024-01-16 16:14:36 +0530

    UI: Add Angular Material and modify textarea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SavaaliiDashboard } from './dashboard/savaalii-dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, SavaaliiDashboard],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/dashboard/savaalii-dashboard.component.ts
+++ src/app/dashboard/savaalii-dashboard.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'savaalii-dashboard',
   templateUrl: './savaalii-dashboard.component.html',
+  styleUrl: './savaalii-dashboard.component.css',
 })
 export class SavaaliiDashboard {
   enteredValue = '';
